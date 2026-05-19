5783af11d5eefb651133b15757dc55e76791979a
shreyanshiBarat <shreyanshi6972@ggn.hxls.org>
2026-05-19 19:56:55 +0530

    Lesson 5

---
+++ Activity1.py
@@ -0,0 +1,7 @@
+a= 1
+if a > 0 :
+ print(a, " is a positive number")
+
+b= -2
+if b > 0 :
+ print(b, " is a positive number")
---
+++ Activity2.py
@@ -0,0 +1,7 @@
+a=float (input("Enter the amount you brought for"))
+b= float (input("Enter the amount you are selling it for"))
+c= b-a
+if a > b:
+ print(a, " is your loss")
+else:
+ print(c, " is your profit")
